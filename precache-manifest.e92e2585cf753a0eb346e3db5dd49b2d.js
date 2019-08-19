6296773407ebf8b6625827d121cc1c823e5e455e
theblackcrown <digidi9@mail.com>
2019-08-19 13:23:25 +0300

    Updates

---
+++ precache-manifest.e92e2585cf753a0eb346e3db5dd49b2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c46b4bd62eab8ba5ed55ac0c9c930540",
+    "url": "/theblackcrown/monsters/index.html"
+  },
+  {
+    "revision": "090bdb7f1515eaf7dd6f",
+    "url": "/theblackcrown/monsters/static/css/main.26e687e7.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/theblackcrown/monsters/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "090bdb7f1515eaf7dd6f",
+    "url": "/theblackcrown/monsters/static/js/main.58f55e7e.chunk.js"
+  },
+  {
+    "revision": "6241788f79b213fc00e4",
+    "url": "/theblackcrown/monsters/static/js/runtime~main.666ecd31.js"
+  }
+]);
